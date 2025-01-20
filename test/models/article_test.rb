8da8afa62182b43fb98a94634d7d9a0209492c06
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2025-01-20 17:41:26 +0000

    7.3.3 Validations and Displaying Error Messages

--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -1,7 +1,13 @@
 require "test_helper"
 
 class ArticleTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should be valid" do
+    article = Article.new(title: "Title", body: "Body is at least 10 chars")
+    assert article.valid?
+  end
+
+  test "should be invalid" do
+    article = Article.new
+    refute article.valid?
+  end
 end
