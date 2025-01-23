4cc1e14a53fdabd383f3cc1f583de047baf2f752
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2025-01-23 16:42:56 +0000

    8 / 9.1 / 9.2 - Adding a second model + refactoring

---
+++ test/controllers/comments_controller_test.rb
@@ -0,0 +1,8 @@
+require "test_helper"
+
+class CommentsControllerTest < ActionDispatch::IntegrationTest
+  # TODO
+  # test "the truth" do
+  #   assert true
+  # end
+end
---
+++ test/models/comment_test.rb
@@ -0,0 +1,8 @@
+require "test_helper"
+
+class CommentTest < ActiveSupport::TestCase
+  # TODO
+  # test "the truth" do
+  #   assert true
+  # end
+end
