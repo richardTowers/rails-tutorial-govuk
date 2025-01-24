08fca2e25e123e7559ae451626f65e94dc35ea8e
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2025-01-24 12:17:41 +0000

    Add tests for comments controller

--- test/controllers/comments_controller_test.rb
+++ test/controllers/comments_controller_test.rb
@@ -1,8 +1,21 @@
 require "test_helper"
 
 class CommentsControllerTest < ActionDispatch::IntegrationTest
-  # TODO
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should create comment" do
+    article = articles(:one)
+    assert_difference -> { article.comments.count } do
+      post article_comments_url(article), params: { comment: { commenter: "Daffy Duck", body: "Quack! Quack! Quack!" } }
+    end
+
+    assert_redirected_to article_url(article)
+  end
+
+  test "should not create invalid comments" do
+    article = articles(:one)
+    assert_no_difference -> { article.comments.count } do
+      post article_comments_url(article), params: { comment: { commenter: "Daffy Duck" } }
+    end
+
+    assert_response :unprocessable_entity
+  end
 end
