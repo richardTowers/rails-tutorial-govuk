dc6d841d3db574ffcd2622726f63ffda6c542e7c
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2025-01-24 14:54:24 +0000

    Use before_action to get article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,11 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
+
   def index
     @articles = Article.all
   end
 
   def show
-    @article = Article.find(params[:id])
     @new_comment = @article.comments.build
   end
 
@@ -22,13 +23,9 @@ class ArticlesController < ApplicationController
     end
   end
 
-  def edit
-    @article = Article.find(params[:id])
-  end
+  def edit; end
 
   def update
-    @article = Article.find(params[:id])
-
     if @article.update(article_params)
       redirect_to @article
     else
@@ -37,7 +34,6 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
 
     redirect_to root_path, status: :see_other
@@ -47,5 +43,9 @@ private
 
   def article_params
     params.require(:article).permit(:title, :body, :status)
+  end
+
+  def set_article
+    @article = Article.find(params[:id])
   end
 end
